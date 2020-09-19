596455465381164625a22f06446908bed21bc4f7
calebdeji <calebdeji06@gmail.com>
2020-09-19 09:38:23 +0100

    test: update useAudio test to work with the return value of useAudio

--- src/hooks/useAudio/useAudio.test.js
+++ src/hooks/useAudio/useAudio.test.js
@@ -1,48 +1,50 @@
-import { renderHook, act, cleanup } from '@testing-library/react-hooks';
+import { renderHook, act, cleanup } from "@testing-library/react-hooks";
 
-import {
-  useAudio,
-} from '../../index';
+import { useAudio } from "../../index";
 
-describe('useAudio', () => {
-  afterEach(cleanup)
+describe("useAudio", () => {
+  afterEach(cleanup);
 
   it("throws an error when no cloud name is provided", async () => {
     const { result } = renderHook(() => useAudio());
-    await act(async () => await expect(result.error).toBeInstanceOf(Error))
+    await act(async () => await expect(result.error).toBeInstanceOf(Error));
   });
 
   it("throws an error when no public_id is provided to getAudio", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useAudio({ cloud_name: "testing-hooks-upload" }));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useAudio({ cloud_name: "testing-hooks-upload" })
+    );
     await act(async () => {
       await expect(() => result.current.getAudio()).toThrow();
-
-    })
+    });
 
     await waitForNextUpdate();
   });
 
   it("updates status to loading when calling getAudio", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useAudio({ cloud_name: "testing-hooks-upload" }));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useAudio({ cloudName: "testing-hooks-upload" })
+    );
     await act(async () => {
-      await result.current.getAudio({ public_id: "game-sounds/switch" })
-      expect(result.current.status).toBe("loading");
+      await result.current.generateUrl({ publicId: "game-sounds/switch" });
+      expect(result.current.isLoading).toBeTruthy();
     });
-
-    await waitForNextUpdate()
-
-    expect(result.current.status).toBe("success");
   });
 
   it("updates data to a Cloudinary URL on successful request", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useAudio({ cloud_name: "testing-hooks-upload" }));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useAudio({ cloudName: "testing-hooks-upload" })
+    );
 
     await act(async () => {
-      result.current.getAudio({ public_id: "game-sounds/switch" })
+      result.current.generateUrl({ publicId: "game-sounds/switch" });
     });
 
-    await act(async () => await waitForNextUpdate())
-
-    expect(result.current.data).toMatch(/https?:\/\/res\.cloudinary\.com\/\S+/)
+    await act(async () => {
+      await waitForNextUpdate();
+      expect(result.current.url).toMatch(
+        /https?:\/\/res\.cloudinary\.com\/\S+/
+      );
+    });
   });
-})
+});
