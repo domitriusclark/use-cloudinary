d0f4601f507bae9c48d150544db8f8ab6bb6ebba
calebdeji <calebdeji06@gmail.com>
2020-09-19 14:46:00 +0100

    fix undefined searchConfigs: change searchConfigs to searchConfig

--- src/hooks/useVideo/useVideo.test.js
+++ src/hooks/useVideo/useVideo.test.js
@@ -1,48 +1,52 @@
-import { renderHook, act, cleanup } from '@testing-library/react-hooks';
+import { renderHook, act, cleanup } from "@testing-library/react-hooks";
 
-import {
-  useVideo,
-} from '../../index';
+import { useVideo } from "../../index";
 
-describe('useVideo', () => {
-  afterEach(cleanup)
+describe("useVideo", () => {
+  afterEach(cleanup);
 
   it("throws an error when no cloud name is provided", async () => {
     const { result } = renderHook(() => useVideo());
-    await act(async () => await expect(result.error).toBeInstanceOf(Error))
+    await act(async () => await expect(result.error).toBeInstanceOf(Error));
   });
 
-  it("throws an error when no public_id is provided to getVideo", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useVideo({ cloud_name: "testing-hooks-upload" }));
+  it("throws an error when no publicId is provided to getVideo", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useVideo({ cloudName: "testing-hooks-upload" })
+    );
     await act(async () => {
-      await expect(() => result.current.getVideo()).toThrow();
-    })
+      await expect(() => result.current.generateUrl()).toThrow();
+    });
 
-    await waitForNextUpdate()
+    await waitForNextUpdate();
   });
 
   it("updates status to loading when calling getVideo", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useVideo({ cloud_name: "testing-hooks-upload" }));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useVideo({ cloudName: "testing-hooks-upload" })
+    );
     await act(async () => {
-      await result.current.getVideo({ public_id: "trees" })
+      await result.current.generateUrl({ publicId: "trees" });
 
-      expect(result.current.status).toBe("loading");
+      expect(result.current.isLoading).toBeTruthy();
     });
 
-    await waitForNextUpdate()
+    await waitForNextUpdate();
 
-    expect(result.current.status).toBe("success");
+    expect(result.current.isSuccess).toBeTruthy();
   });
 
   it("updates data to a Cloudinary URL on successful request", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useVideo({ cloud_name: "testing-hooks-upload" }));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useVideo({ cloudName: "testing-hooks-upload" })
+    );
 
     await act(async () => {
-      result.current.getVideo({ public_id: "trees" })
+      result.current.generateUrl({ publicId: "trees" });
     });
 
-    await act(async () => await waitForNextUpdate())
+    await act(async () => await waitForNextUpdate());
 
-    expect(result.current.data).toMatch(/https?:\/\/res\.cloudinary\.com\/\S+/)
+    expect(result.current.url).toMatch(/https?:\/\/res\.cloudinary\.com\/\S+/);
   });
-})
+});
