f27f4864b5a00458d1fa754dcf41a37626270c9b
DomitriusAnthony <domitriusaclark@gmail.com>
2020-11-03 02:33:20 -0500

    Removing unused hook exports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import useImage from './hooks/useImage'
-import useUpload from './hooks/useUpload'
 import useVideo from './hooks/useVideo'
 import useGif from './hooks/useGif'
-import useSearch from './hooks/useSearch'
 import useAudio from './hooks/useAudio';
-import useCloudinary from './hooks/useCloudinary';
+
 
 export {
   useImage,
-  useUpload,
   useVideo,
   useGif,
-  useSearch,
   useAudio,
-  useCloudinary
 }
